2186c9dda043f1bb33ad5192d20c5a7111fff016
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2019-04-18 11:16:56 -0300

    Cadastro de Consultas realizadas e cadastro de usuarios iniciado

--- src/Pages/Cadastros/cadastros.js
+++ src/Pages/Cadastros/cadastros.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import ApiService from '../../Services/ApiService';
+import Axios from 'axios';
 
 const URL   = 'http://localhost:5001/api/Medicos';
 class Cadastros extends Component{
     constructor(){
         super();
         this.state = {
-            crmMedico: "",
+            CrmMedico: "",
             dataConsulta: "",
             statusConsulta: 1,
             idProntuario: "",
             listaStatusConsulta: [],
-            listaProntuario: []  ,
+            listaProntuario: [] ,
             listaMedicos: []     
-        }
-     
+        }     
     }
     componentDidMount(){
         this.buscarMedicos();
-        
+
+        ApiService
+        .call('Medicos')
+        .getAll()
+        .then(data => {
+            this.setState({listaMedicos : data});
+        });
     }
     atualizaIdProntuario(event){
         this.setState ({idProntuario : event.target.value})
     }
-    
+    atualizaDataConsulta(event){
+        this.setState ({dataConsulta : event.target.value})
+    }
     buscarMedicos(){
         fetch(URL,
             {
@@ -37,44 +45,80 @@ class Cadastros extends Component{
            .then(data => this.setState({ listaMedicos : data }))
            .catch(erro => console.log(erro))
        }
-
        atualizaEstadoCrmMedico(event){
-        this.setState({crmMedico : event.target.value});
+        this.setState({CrmMedico : event.target.value});
     }
-
     cadastrarConsulta(event){
         event.preventDefault();
-
-        ApiService
-        .call('Consultas')
-        .create( {
-            crmMedico : this.state.crmMedico,
-            dataConsulta: this.state.dataConsulta ,
-            statusConsulta: this.state.statusConsulta,
-            idProntuario: this.state.idProntuario
-        })
-        .then(data => { if(data.status === 200){
-            console.log(data + "Consulta cadastrada com sucesso");}
-            else{
-                alert("Dados preenchidos incorretamente!");
+        
+        fetch('http://localhost:5001/api/Consultas', {
+            method: 'POST',
+            body: JSON.stringify({ CrmMedico: this.state.CrmMedico,
+                dataConsulta: this.state.dataConsulta ,
+                statusConsulta: 1,
+                idProntuario: this.state.idProntuario}),
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization" : 'Bearer ' + localStorage.getItem("usuario-spmedgroup") 
             }
-        })
+        }
+        )        
+        .then(resposta => resposta)
         .catch( erro => console.log(erro))
+        console.log(this.state.CrmMedico);
     }
+
+    cadastrarUsuario(event){
+        event.preventDefault();
+
+        let usuario = {
+            nome: this.state.Nome,
+            email: this.state.Email,
+            senha: this.state.Senha,
+            dataNascimento: this.state.dataNascimento,
+            idTipoUsuario: 1
+        }
+
+        Axios.post('http://localhost:5001/api/Usuarios');
+
+        console.log(usuario);
+    }
+    atualizaEstadoDataNascimento(event){
+        this.setState({dataNascimento: event.target.value})
+    }
+    atualizaEstadoNome(event){
+        this.setState({nome: event.target.value})
+    }
+    atualizaEstadoDataEmail(event){
+        this.setState({email: event.target.value})
+    }
+    atualizaEstadoDataSenha(event){
+        this.setState({senha: event.target.value})
+    }
+
     render(){
         return(
             <div>
                 <form onSubmit = {this.cadastrarConsulta.bind(this)}>
-                    <input type="date" placeholder="dd/mm/yyyy" value={this.state.dataConsulta}/>
+                    <input type="date" placeholder="dd/mm/yyyy" onChange={this.atualizaDataConsulta.bind(this)} value={this.state.dataConsulta}/>
                     <input type="text" placeholder="Digite o id do prontuário" onChange={this.atualizaIdProntuario.bind(this)} value={this.state.idProntuario}/>                                      
-                    <select  value={this.state.crmMedico}  onChange={this.atualizaEstadoCrmMedico.bind(this)}>
-                    <option value="0" disabled>Selecione : </option>
+                    <select  value={this.state.CrmMedico}  onChange={this.atualizaEstadoCrmMedico.bind(this)}>
+                    <option  >Selecione : </option>
                         {
                             this.state.listaMedicos.map((element) => {
-                                return <option key={element.crm} value={element.crm}>{element.crm}</option>
+                                return <option key={element.crm} value={element.crm}>{element.idUsuarioNavigation.nome}</option>
                             })
                     }
                     </select>
+                    <button>Cadastrar</button>
+                </form>
+                
+                {/* Form para cadastrar Usuarios */}
+                <form onSubmit = {this.cadastrarUsuario.bind(this)}>
+                    <input type="text" placeholder="Nome do Paciente" onChange={this.atualizaEstadoNome.bind(this)} value={this.state.Nome}/>                                      
+                    <input type="text" placeholder="Senha" onChange={this.atualizaEstadoSenha.bind(this)} value={this.state.Senha}/>                                      
+                    <input type="text" placeholder="Email" onChange={this.atualizaEstadoEmail.bind(this)} value={this.state.Email}/>                                      
+                    <input type="date" placeholder="dd/mm/yyyy" onChange={this.atualizaEstadoDataNascimento.bind(this)} value={this.state.Nascimento}/>                                       
                     <button>Cadastrar</button>
                 </form>
             </div>
